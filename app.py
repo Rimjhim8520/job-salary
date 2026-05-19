47ef3184922c5d74d157d743277bc6daa575ca9d
Rimjhim Kumari <rimjhimkumari58457@gmail.com>
2026-05-19 12:37:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ if st.button("Predict Salary"):
 import streamlit as st
 import pickle
 import pandas as pd
-from twilio.rest import Client
+'''from twilio.rest import Client'''
 
 # =========================
 # PAGE CONFIG
@@ -606,62 +606,6 @@ else:
                 file_name="salary_receipt.csv",
                 mime="text/csv"
             )
-                 # prediction inputs...
-
-    # =========================
-    # WHATSAPP SECTION
-    # =========================
-    st.markdown("---")
-
-    st.subheader("📲 Send Result to WhatsApp")
-
-    phone_number = st.text_input(
-        "Enter WhatsApp Number with Country Code",
-        placeholder="+919876543210"
-    )
-
-    if st.button("Send to WhatsApp"):
-
-        if phone_number == "":
-
-            st.warning("Please Enter WhatsApp Number")
-
-        else:
-
-            message = f'''
-💼 Salary Prediction Result
-
-👤 User: {st.session_state.username}
-
-📌 Experience: {exp} 
-🛠 Skills: {skills}
-📜 Certifications: {cert}
-
-💼 Job Role: {job}
-🎓 Education: {edu}
-📍 Location: {loc}
-🏭 Industry: {ind}
-
-💰 Predicted Salary: ₹ {predicted_salary:,}
-🚀 Thank You for using Salary Prediction App
-'''
-        try:
-
-                # SEND WHATSAPP MESSAGE
-                kit.sendwhatmsg_instantly(
-                    phone_number,
-                    message,
-                    wait_time=15,
-                    tab_close=True
-                )
-
-                st.success(
-                    "✅ WhatsApp Message Sent Successfully"
-                )
-
-        except Exception as e:
-
-                st.error(f"Error: {e}")
 
     # =========================
     # DASHBOARD PAGE
